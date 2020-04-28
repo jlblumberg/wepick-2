6462e5100df3e1146915bbf0ca8a45dc1add0e75
Jason <jasylwong@gmail.com>
2020-04-28 12:06:01 +0100

    add test for running server

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,11 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+
+
 class WePick {
 
 }
--- backend/server.test.js
+++ backend/server.test.js
@@ -1,5 +1,6 @@
 import { expect } from "chai";
 import WePick from "./server.js";
+import request from "supertest";
 
   describe("Server", function() {
     let testServer = new WePick
@@ -7,3 +8,13 @@ import WePick from "./server.js";
       expect(testServer).to.be.an('object');
     });
   });
+
+  describe("Server running test", function() {
+    it("runs the server", function(done) {
+      const res = request(WePick).get("/")
+
+      res.expect({val: "Server running"})
+      res.expect(200, done)
+    })
+  })
+  
