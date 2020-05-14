d5bab4f53e30d8a7e2570c5710bfd96fedc9f27d
Joe Ellingworth <joeellingworth@gmail.com>
2020-05-14 12:08:03 +0100

    fixed typo in register.js

--- backend/validation/register.js
+++ backend/validation/register.js
@@ -1,6 +1,7 @@
 const Validator = require("validator");
 const isEmpty = require("is-empty");
 
+
 module.exports = function validateRegisterInput(data){
   let errors = {};
 
@@ -30,7 +31,7 @@ module.exports = function validateRegisterInput(data){
     errors.password2 = "Confirm password field is required";
   }
 
-  if (Validator.isLength(data.password, { min: 6, max: 30})){
+  if (!Validator.isLength(data.password, { min: 6, max: 30})){
     errors.password = "Password must be at least 6 characters";
   }
 
