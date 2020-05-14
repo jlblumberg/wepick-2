c680a95ac4c40f86652e12becd323e3c928d1755
Josh Blumberg <jlblumberg@gmail.com>
2020-05-14 11:51:05 +0100

    Add routes to server

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const passport = require("passport");
 
+const users = require("./routes/api/users");
 const app = express();
 
-//Bodyparser middleware
+// Bodyparser middleware
 app.use(
   bodyParser.urlencoded({
     extended: false
@@ -12,7 +14,7 @@ app.use(
 );
 app.use(bodyParser.json());
 
-//DB Config
+// DB Config
 const db = require("./config/keys");
 
 // Connect to MongoDB
@@ -23,6 +25,13 @@ mongoose
   )
   .then(() => console.log("MongoDB successfully connected"))
   .catch(err => console.log(err));
+
+// Passport middleware
+app.use(passport.initialize());
+// Passport config
+require("./config/passport")(passport);
+// Routes
+app.use("/api/users", users);
 
 const listen = app.listen(db.port, () => console.log(`Server running on port ${db.port}`));
 
