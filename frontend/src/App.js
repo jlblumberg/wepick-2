be14745c8e7535e2885a57c908981c44b79811e7
Josh Blumberg <jlblumberg@gmail.com>
2020-05-26 11:54:04 +0100

    Remove classnames from app component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,10 +3,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>wepick2</h1>
-      </header>
+    <div>
+      <h1>wepick2</h1>
     </div>
   );
 }
