6dd64cb7235a51e1ac06e89feb23ed28cff944a5
Joe Ellingworth <joeellingworth@gmail.com>
2020-06-23 11:05:43 +0100

    added styling for signup button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,12 @@ import React from 'react';
 import './App.css';
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
-import Register from './components/auth/Register';
 
 function App() {
   return (
     <div>
       <Navbar />
       <Landing />
-      <Register/>
     </div>
   );
 }
--- frontend/src/components/auth/Register.js
+++ frontend/src/components/auth/Register.js
@@ -8,54 +8,63 @@ function Register() {
     email: "",
     password: "",
     password2: "",
-    errors: { name: "", email: "", password: "", password2: ""}
+    errors: { name: "", email: "", password: "", password2: "" }
   })
 
   const onChange = (e) => {
-    setFormValues({...formValues, [e.target.id]: e.target.value})
+    setFormValues({ ...formValues, [e.target.id]: e.target.value })
   }
 
-  return(
+  return (
     <div>
       <BrowserRouter>
         <p id="alreadyregistered">Already have an account? <Link id="login" to="/login">Log In</Link></p>
       </BrowserRouter>
       <form>
         <div>
-        <label htmlFor="name">Name</label>
-        <input 
-          id='name'
-          value={formValues.name}
-          onChange={onChange}
-          type="text"
-          errors={formValues.errors.name}
-        />
-        <label htmlFor="email">Email</label> 
-        <input 
-          id='email'
-          value={formValues.email}
-          onChange={onChange}
-          type="email"
-          errors={formValues.errors.email}
-        />
-        <label htmlFor="password">Password</label> 
-        <input 
-          id='password'
-          value={formValues.password}
-          onChange={onChange}
-          type="password"
-          errors={formValues.errors.password}
-        />
-        <label htmlFor="password2">Confirm Password</label> 
-        <input 
-          id='password2'
-          value={formValues.password2}
-          onChange={onChange}
-          type="password"
-          errors={formValues.errors.password2}
-        />
+          <label htmlFor="name">Name</label>
+          <input
+            id='name'
+            value={formValues.name}
+            onChange={onChange}
+            type="text"
+            errors={formValues.errors.name}
+          />
+          <label htmlFor="email">Email</label>
+          <input
+            id='email'
+            value={formValues.email}
+            onChange={onChange}
+            type="email"
+            errors={formValues.errors.email}
+          />
+          <label htmlFor="password">Password</label>
+          <input
+            id='password'
+            value={formValues.password}
+            onChange={onChange}
+            type="password"
+            errors={formValues.errors.password}
+          />
+          <label htmlFor="password2">Confirm Password</label>
+          <input
+            id='password2'
+            value={formValues.password2}
+            onChange={onChange}
+            type="password"
+            errors={formValues.errors.password2}
+          />
         </div>
-        <button id="sign-up" type="submit">Sign up</button>
+        <button id="sign-up"
+          style={{
+            width: "150px",
+            borderRadius: "3px",
+            letterSpacing: "1.5px",
+            marginTop: "1rem"
+          }}
+          type="submit"
+          className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+        >Sign up</button>
       </form>
     </div>
   )
