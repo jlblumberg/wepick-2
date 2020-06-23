93d35f51da0c6969479d58de9c6ad6e2b18adbf3
Josh Blumberg <jlblumberg@gmail.com>
2020-06-23 11:46:23 +0100

    Add login field for email

--- frontend/src/__tests__/Login.test.js
+++ frontend/src/__tests__/Login.test.js
@@ -7,15 +7,21 @@ import Login from '../components/auth/Login';
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('Login', () => {
-  const wrapper = shallow(<Login/>);
+  const wrapper = shallow(<Login />);
 
   it('renders correctly', () => {
     expect(wrapper).toMatchSnapshot();
   });
 
-  it('should have a signup link if you don\'t already have an account', () => {
-    expect(wrapper.find('#notregistered').text()).toContain('Don\'t have an account?')
-  })
+  it('has a signup link if you don\'t already have an account', () => {
+    expect(wrapper.find('#notregistered').text()).toContain('Don\'t have an account?');
+    expect(wrapper.find('#signup').text()).toContain('Sign up');
+  });
 
+  it('has a field for email', () => {
+    expect(wrapper.find('input#email').prop('value')).toEqual("");
+    expect(wrapper.find('input#email').prop('type')).toEqual("email");
+    expect(wrapper.find('input#email').prop('errors')).toEqual("");
+  });
 
 });
--- frontend/src/components/auth/Login.js
+++ frontend/src/components/auth/Login.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter, Link } from 'react-router-dom';
 
 function Login() {
+
+  const [formValues, setFormValues] = useState({
+    email: "",
+    password: "",
+    errors: { email: "", password: "" }
+  })
+
+  const onChange = (e) => {
+    setFormValues({ ...formValues, [e.target.id]: e.target.value })
+  }
+
   return (
     <div>
       <BrowserRouter>
-        <p id="notregistered">Don't have an account? <Link id="login" to="/login">Log In</Link></p>
+        <p id="notregistered">Don't have an account? <Link id="signup" to="/signup">Sign up</Link></p>
       </BrowserRouter>
+      <form>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            id='email'
+            value={formValues.email}
+            onChange={onChange}
+            type="email"
+            errors={formValues.errors.email}
+          />
+        </div>
+      </form>
     </div>
   )
 }
