8bbf2b90529781724b254dc74f68094606c69d9c
Jason <jasylwong@gmail.com>
2020-07-07 11:21:47 +0100

    pass signup routing test

--- frontend/src/__tests__/Router.test.js
+++ frontend/src/__tests__/Router.test.js
@@ -1,39 +1,34 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { act } from 'react-dom/test-utils';
-import Enzyme from 'enzyme';
+import Enzyme, { mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import { render } from "react-dom";
-import { mount, shallow } from 'enzyme';
-import { MemoryRouter } from 'react-router-dom'
+import { MemoryRouter, Router } from 'react-router-dom'
 import Landing from '../components/layout/Landing'
 import App from '../App'
-import Login from '../components/auth/Login'
+import { createMemoryHistory } from 'history'
+import { render as renderTest, fireEvent } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
 
 Enzyme.configure({ adapter: new Adapter() });
 
-test('Landing page is the default page', () => {
-  const wrapper = mount(
-    <MemoryRouter initialEntries={[ '/' ]}>
-      <App/>
-    </MemoryRouter>
-  );
-  expect(wrapper.find(Landing)).toHaveLength(1);
+describe('Router', () => {
+  test('Landing page is the default page', () => {
+    const wrapper = mount(
+      <MemoryRouter initialEntries={[ '/' ]}>
+        <App/>
+      </MemoryRouter>
+    );
+    expect(wrapper.find(Landing)).toHaveLength(1);
+  })
+
+  test('allows linking from landing to signup page', () => {
+    const history = createMemoryHistory()
+    const { container, getByText } = renderTest(
+      <Router history={history}>
+        <App />
+      </Router>
+    )
+    expect(container.innerHTML).toMatch('Sign Up')
+    fireEvent.click(getByText(/sign up/i))
+    expect(container.innerHTML).toMatch('Name')
+  })
 })
-
-test('Signup routing', () => {
-  const root = document.createElement('div');
-  document.body.appendChild(root);
-
-  render (
-    <MemoryRouter initialEntries={[ '/' ]}>
-      <Landing />
-    </MemoryRouter>,
-    root
-  );
-  act(() => {
-    const goToSignup = document.querySelector('#signup')
-    goToSignup.dispatchEvent(new MouseEvent("click", { bubbles: "true" }))
-  });
-  expect(document.querySelector('#name').textContent).toBe('Name')
-});
--- frontend/src/components/layout/Landing.js
+++ frontend/src/components/layout/Landing.js
@@ -2,34 +2,38 @@ import React from 'react';
 import { BrowserRouter, Link } from 'react-router-dom';
 
 function Landing() {
-  return(
+  return (
     <div style={{ height: "75vh" }} className="container mt-10 center-align">
-      <BrowserRouter>
-        <Link 
-          id='signup'
-          to='/signup'
-          style={{
-            width: "140px",
-            borderRadius: "3px",
-            letterSpacing: "1.5px"
-          }}
-          className="btn btn-large waves-effect waves-light hoverable blue accent-3"
-        >
-          Sign Up
+      <a 
+        href="/signup"
+        id="signuptest"
+      >
+        Signup test
+      </a>
+      <Link
+        id='signup'
+        to='/signup'
+        style={{
+          width: "140px",
+          borderRadius: "3px",
+          letterSpacing: "1.5px"
+        }}
+        className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+      >
+        Sign Up
         </Link>
-        <Link 
-          id='login'
-          to='/login'
-          style={{
-            width: "140px",
-            borderRadius: "3px",
-            letterSpacing: "1.5px"
-          }}
-          className="btn btn-large waves-effect waves-light hoverable blue accent-3"
-        >
-          Log In
+      <Link
+        id='login'
+        to='/login'
+        style={{
+          width: "140px",
+          borderRadius: "3px",
+          letterSpacing: "1.5px"
+        }}
+        className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+      >
+        Log In
         </Link>
-      </BrowserRouter>
     </div>
   )
 }
