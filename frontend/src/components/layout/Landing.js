52c8091058ff22127f219d16c8716563ebc08bd6
Josh Blumberg <jlblumberg@gmail.com>
2020-07-09 09:52:42 +0100

    Remove test link in app.js

--- frontend/src/components/layout/Landing.js
+++ frontend/src/components/layout/Landing.js
@@ -1,15 +1,9 @@
 import React from 'react';
-import { BrowserRouter, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function Landing() {
   return (
     <div style={{ height: "75vh" }} className="container mt-10 center-align">
-      <a 
-        href="/signup"
-        id="signuptest"
-      >
-        Signup test
-      </a>
       <Link
         id='signup'
         to='/signup'
